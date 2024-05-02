ef8349618314ba494e9f104b54a63691504305d1
Idelilah <ruthtirusew944@gmail.com>
2024-05-02 14:35:13 +0300

    fix: image rendering issue add: purchase order edit and dynamic field edits

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ import { withI18nMiddleware } from '@/middlewares/locale.middleware'
 export default chain([withAuthMiddleware, withI18nMiddleware])
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)']
 }
