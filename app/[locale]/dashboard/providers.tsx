f2c91f956fe7b1485c8df51486454319d2bf25e2
Idelilah <ruthtirusew944@gmail.com>
2024-05-26 14:13:48 +0300

    fixed....?

--- app/[locale]/dashboard/providers.tsx
+++ app/[locale]/dashboard/providers.tsx
@@ -1,8 +1,29 @@
 'use client'
 import {ThemeProvider} from 'next-themes';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 
 export function Provider({children}: {children:React.ReactNode}){
+  const router = useRouter()
+
+
+  useEffect(() =>{
+      const fetchUser = async () => {
+        try {
+          const res = await axios.get(`/api/auth/me`)
+          if(res?.data?.company_id === null){
+            router.push("/onboarding")
+          }
+        } catch (error) {
+          
+        }
+    }
+
+    }, [])
+    
+
     return(
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
         {children}
